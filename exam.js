dbdb9976e0a681f44126c65d94f24f7bd34c00eb
onlineexamvzm25 <onlineexamvzm25@gmail.com>
2025-04-02 18:28:52 +0530

    Add files via upload

--- exam.js
+++ exam.js
@@ -216,7 +216,7 @@ document.getElementById("download-pdf").addEventListener("click", function(event
   const resmarks = "Correct : " + CorrectdCount;
   const wrmarks = "Wrong : " + WrongCount;
   const examresult = "Note : Exam Result/State Ranks of all students will be provided tomorrow";
-  const contactdet = "For more details contact/watsup on 8125250125 ";
+  const contactdet = "For more details contact/whatsapp on 8125250125 ";
 
   // Apply replaceSymbols to basic information
   content += `<h2>${replaceSymbols(exname)}</h2>`;
